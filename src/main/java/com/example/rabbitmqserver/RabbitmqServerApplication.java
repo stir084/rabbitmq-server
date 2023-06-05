633894b76d037c1ebc5d1c6f41f71e47c1ff1e73
DESKTOP-1UKM23C\jun <wnsgh1357@naver.com>
2023-06-05 12:08:19 +0900

    first commit

---
+++ src/main/java/com/example/rabbitmqserver/RabbitmqServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqServerApplication.class, args);
+	}
+
+}
